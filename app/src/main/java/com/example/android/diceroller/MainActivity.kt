9c1c845451ed7f9c48de423c863abdde8c1581f4
jaimgarod10 <jaimgarod@gmail.com>
2020-10-18 10:54:49 +0200

    modificado

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -16,39 +16,44 @@
 
 package com.example.android.diceroller
 
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import kotlinx.android.synthetic.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    //definimos ImageView
     lateinit var diceImage: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //definimos el boton tirar dado
         val rollButton: Button = findViewById(R.id.roll_button)
+        //llama a la función rollDice cuando se hace click en el boton de tirar
         rollButton.setOnClickListener {
             rollDice()
         }
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage = findViewById(R.id.dice_image)//se carga el ImageView
+        diceImage.setImageResource(R.drawable.dado)//se muestra una imagen dentro del ImageView
     }
 
     private fun rollDice() {
+        //generamos un numero aleatorio con la función random
         val randomInt = Random().nextInt(6) + 1
+        //dependiendo del numero que salga de la función random se muestra una imagen diferente
         val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> diceImage.setImageResource(R.drawable._1)
+            2 -> diceImage.setImageResource(R.drawable._2)
+            3 -> diceImage.setImageResource(R.drawable._3)
+            4 -> diceImage.setImageResource(R.drawable._4)
+            5 -> diceImage.setImageResource(R.drawable._5)
+            else -> diceImage.setImageResource(R.drawable._6)
         }
-
-        diceImage.setImageResource(drawableResource)
     }
 }
